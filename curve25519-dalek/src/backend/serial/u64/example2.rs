dfe941d214b00636f55dd4cfff370657d120cf78
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-07-22 10:07:57 +0100

    Simplify

--- curve25519-dalek/src/backend/serial/u64/example2.rs
+++ curve25519-dalek/src/backend/serial/u64/example2.rs
@@ -7,14 +7,13 @@ use vstd::arithmetic::mul::*;
 verus! {
 
 
-        pub struct Scalar52 {
+        struct Scalar52 {
             // ADAPTED CODE LINE: we give a name to the field: "limbs"
             pub limbs: [u64; 5],
         }
 
         #[verifier::allow_in_spec]
-        #[inline(always)]
-        pub fn m(x: u64, y: u64) -> (z: u128)
+        fn m(x: u64, y: u64) -> (z: u128)
         requires
             x < (1u64 << 52),
             y < (1u64 << 52),
@@ -47,10 +46,7 @@ verus! {
             (x as u128) * (y as u128)
         }
 
-    /// Compute `a^2`
-    #[inline(always)]
-    #[rustfmt::skip] // keep alignment of calculations
-    pub (crate) fn square_internal(a: &Scalar52) -> (res: [u128; 9])
+    fn square_internal(a: &Scalar52) -> (res: [u128; 9])
     requires
         forall|i: int| 0 <= i < 5 ==> a.limbs[i] < (1u64 << 52),
     {
@@ -70,14 +66,6 @@ verus! {
             assert( m(3, 3) < 10 );
         }
         z[1] = m(a.limbs[0], a.limbs[1]) * 2;
-        assume(false);
-        z[2] = m(a.limbs[0], a.limbs[2]) * 2 + m(a.limbs[1], a.limbs[1]);
-        z[3] = m(a.limbs[0], a.limbs[3]) * 2 + m(a.limbs[1], a.limbs[2]) * 2;
-        z[4] = m(a.limbs[0], a.limbs[4]) * 2 + m(a.limbs[1], a.limbs[3]) * 2 + m(a.limbs[2], a.limbs[2]);
-        z[5] =                 m(a.limbs[1], a.limbs[4]) * 2 + m(a.limbs[2], a.limbs[3]) * 2;
-        z[6] =                                 m(a.limbs[2], a.limbs[4]) * 2 + m(a.limbs[3], a.limbs[3]);
-        z[7] =                                                 m(a.limbs[3], a.limbs[4]) * 2;
-        z[8] =                                                                 m(a.limbs[4], a.limbs[4]);
 
         z
     }

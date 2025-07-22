1345d89295c792d8f34683ad40cb7f83cf76f502
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-07-22 10:14:24 +0100

    Format

--- curve25519-dalek/src/backend/serial/u64/example2.rs
+++ curve25519-dalek/src/backend/serial/u64/example2.rs
@@ -3,20 +3,18 @@ use vstd::prelude::*;
 
 verus! {
 
-
-
-        #[verifier::allow_in_spec]
-        fn m(x: u64, y: u64) -> (z: u128)
-        requires
-            x < (1u64 << 52),
-            y < (1u64 << 52),
-        ensures
-            z < (1u128 << 104),
-        returns (x * y) as u128
-        {
-            assume(false); // Omit proof for brevity
-            (x as u128) * (y as u128)
-        }
+    #[verifier::allow_in_spec]
+    fn m(x: u64, y: u64) -> (z: u128)
+    requires
+        x < (1u64 << 52),
+        y < (1u64 << 52),
+    ensures
+        z < (1u128 << 104),
+    returns (x * y) as u128
+    {
+        assume(false); // Omit proof for brevity
+        (x as u128) * (y as u128)
+    }
 
     fn square_internal(a: u64, b: u64) -> u128
     requires
@@ -25,9 +23,9 @@ verus! {
     {
 
         proof!{
-            assert (a < (1u64 << 52));
-            assert (b < (1u64 << 52));
-            assert (m(a, b) < (1u128 << 104));
+            assert(a < (1u64 << 52));
+            assert(b < (1u64 << 52));
+            assert(m(a, b) < (1u128 << 104));
         }
         m(a, b)
     }

ecc53091090d25692f9581153a94a8ddaff5cd1f
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-07-22 09:38:49 +0100

    Sort of get sum2 working

--- curve25519-dalek/src/backend/serial/u64/example.rs
+++ curve25519-dalek/src/backend/serial/u64/example.rs
@@ -1,6 +1,8 @@
 use vstd::prelude::*;
 
 verus!{
+
+#[verifier::allow_in_spec]
 fn sum(a: u64, b: u64) -> u64
     requires a < 10 && b < 10
     returns (a+b) as u64
@@ -8,6 +10,16 @@ fn sum(a: u64, b: u64) -> u64
   return a+b;
 }
 
+fn sum2(a: u64, b: u64) -> (z: u64)
+    requires a < 10 && b < 10
+    //ensures z == sum(a, b)
+    //ensures 4 == sum(2, 2)
+{
+    let z = a + b;
+    assert(4 == sum(2, 2));
+    //return z;
+    return 0;
+}
 
 }
 

063f5c0197936fc361b2d8af9b14e70a23759482
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-07-22 10:03:28 +0100

    Some more debugging

--- curve25519-dalek/src/backend/serial/u64/example.rs
+++ curve25519-dalek/src/backend/serial/u64/example.rs
@@ -1,3 +1,6 @@
+// So far it seems like the fix is always to put in
+// the preconditions in the ensures clause, and then
+// the panic stops. But this doesn't seem to work for mul
 use vstd::prelude::*;
 
 verus!{
@@ -10,13 +13,40 @@ fn sum(a: u64, b: u64) -> u64
   return a+b;
 }
 
+#[verifier::allow_in_spec]
+fn first(a: u64, b: u64) -> u64
+    returns a
+{
+  return a;
+}
+
+fn sum4(a: u64, b: u64) -> (z: u64)
+{
+    let z = first(a, b);
+    assert(z == first(a, b));
+    return 0;
+}
+
 fn sum2(a: u64, b: u64) -> (z: u64)
     requires a < 10 && b < 10
     //ensures z == sum(a, b)
     //ensures 4 == sum(2, 2)
 {
     let z = a + b;
-    assert(z == sum(a, b));
+    //assert(z == sum(a, 12));
+    //assert(4 == sum(2, 2));
+    //return z;
+    return 0;
+}
+
+fn sum3(a: [u64; 5]) -> (z: u64)
+    requires forall|i: int| a[i] < 10
+    //ensures a[0] < 10,
+    //        a[1] < 10
+    //ensures 4 == sum(2, 2)
+{
+    let z = a[0] + a[1];
+    assert(z == sum(a[0], a[1]));
     //assert(4 == sum(2, 2));
     //return z;
     return 0;

1d0b472af9deec09a77ac3972bd073cc50085fc7
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-08-01 15:33:41 +0100

    Format

--- curve25519-dalek/src/scalar_verus.rs
+++ curve25519-dalek/src/scalar_verus.rs
@@ -1,37 +1,20 @@
-
-
 use vstd::prelude::*;
 
-
-
-
-
 verus! {
-
-
 
 pub struct Scalar {
     pub bytes: [u8; 1],
 }
 
-
 impl Scalar {
-
-    pub const ONE: Self = Self {
-        bytes: [
-            1,
-        ],
-    };
-
+    pub const ONE: Self = Self { bytes: [1] };
 
     pub fn invert(&self) -> Scalar {
         Scalar::ONE
     }
-
 }
 
-fn main()
-    {
-
-    }
+fn main() {
 }
+
+} // verus!

cf51377e8853d21a543772dc57e4d8b6af2f5495
astefano <lacramioara.astefanoaei@gmail.com>
2025-10-03 05:44:56 +0200

    remove wrapper lemma_shr_mono_u64

--- curve25519-dalek/src/backend/serial/u64/field_verus.rs
+++ curve25519-dalek/src/backend/serial/u64/field_verus.rs
@@ -281,7 +281,7 @@ impl FieldElement51 {
         proof {
             // Prove q <= 2 after first iteration
             //lemma_add_preserves_bound(limbs[0], 1u64 << 52, 19);
-            lemma_shr_mono_u64((limbs[0] + 19) as u64, ((1u64 << 52) + 19) as u64, 51);
+            lemma_shr_le_u64((limbs[0] + 19) as u64, ((1u64 << 52) + 19) as u64, 51);
             assert((((1u64 << 52) + 19) as u64) >> 51 == 2) by (compute);
             assert(q <= 2);
         }
@@ -295,7 +295,7 @@ impl FieldElement51 {
         q = (limbs[1] + q) >> 51;
         
         proof {
-            lemma_shr_mono_u64((limbs[1] + old_q) as u64, ((1u64 << 52) + 2) as u64, 51);
+            lemma_shr_le_u64((limbs[1] + old_q) as u64, ((1u64 << 52) + 2) as u64, 51);
             assert(((limbs[1] + old_q) as u64) >> 51 == q);
             assert((((1u64 << 52) + 2) as u64) >> 51 == 2) by (compute);
             assert(q <= 2);
@@ -309,7 +309,7 @@ impl FieldElement51 {
         q = (limbs[2] + q) >> 51;
         
         proof {
-            lemma_shr_mono_u64((limbs[2] + old_q2) as u64, ((1u64 << 52) + 2) as u64, 51);
+            lemma_shr_le_u64((limbs[2] + old_q2) as u64, ((1u64 << 52) + 2) as u64, 51);
             assert(((limbs[2] + old_q2) as u64) >> 51 == q);
             assert((((1u64 << 52) + 2) as u64) >> 51 == 2) by (compute);
             assert(q <= 2);
@@ -323,7 +323,7 @@ impl FieldElement51 {
         q = (limbs[3] + q) >> 51;
         
         proof {
-            lemma_shr_mono_u64((limbs[3] + old_q3) as u64, ((1u64 << 52) + 2) as u64, 51);
+            lemma_shr_le_u64((limbs[3] + old_q3) as u64, ((1u64 << 52) + 2) as u64, 51);
             assert(((limbs[3] + old_q3) as u64) >> 51 == q);
             assert((((1u64 << 52) + 2) as u64) >> 51 == 2) by (compute);
             assert(q <= 2);
@@ -357,7 +357,7 @@ impl FieldElement51 {
         
         proof {
             // limbs[0] < 2^52 + 19, so limbs[0] >> 51 <= 2
-            lemma_shr_mono_u64(limbs[0], ((1u64 << 52) + 19) as u64, 51);
+            lemma_shr_le_u64(limbs[0], ((1u64 << 52) + 19) as u64, 51);
             assert((((1u64 << 52) + 19) as u64) >> 51 == 2) by (compute);
             assert(limbs[0] >> 51 <= 2);
             assert((1u64 << 52) + 2 <= u64::MAX) by (compute);
@@ -366,7 +366,7 @@ impl FieldElement51 {
         limbs[0] &= low_51_bit_mask;
         
         proof {
-            lemma_shr_mono_u64(limbs[1], ((1u64 << 52) + 2) as u64, 51);
+            lemma_shr_le_u64(limbs[1], ((1u64 << 52) + 2) as u64, 51);
             assert((((1u64 << 52) + 2) as u64) >> 51 == 2) by (compute);
             assert(limbs[1] >> 51 <= 2);
             assert((1u64 << 52) + 2 <= u64::MAX) by (compute);
@@ -375,14 +375,14 @@ impl FieldElement51 {
         limbs[1] &= low_51_bit_mask;
         
         proof {
-            lemma_shr_mono_u64(limbs[2], ((1u64 << 52) + 2) as u64, 51);
+            lemma_shr_le_u64(limbs[2], ((1u64 << 52) + 2) as u64, 51);
             assert(limbs[2] >> 51 <= 2);
         }
         limbs[3] += limbs[2] >> 51;
         limbs[2] &= low_51_bit_mask;
         
         proof {
-            lemma_shr_mono_u64(limbs[3], ((1u64 << 52) + 2) as u64, 51);
+            lemma_shr_le_u64(limbs[3], ((1u64 << 52) + 2) as u64, 51);
             assert(limbs[3] >> 51 <= 2);
         }
         limbs[4] += limbs[3] >> 51;

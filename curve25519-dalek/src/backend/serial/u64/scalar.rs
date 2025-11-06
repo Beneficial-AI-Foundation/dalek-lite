0dfcb3047ef9595d2c6fe0d8e8c1f0693349b260
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-11-06 15:41:12 +0000

    Bring back proof of m

--- curve25519-dalek/src/backend/serial/u64/scalar.rs
+++ curve25519-dalek/src/backend/serial/u64/scalar.rs
@@ -14,6 +14,9 @@ use super::subtle_assumes::select;
 use core::fmt::Debug;
 use core::ops::{Index, IndexMut};
 use subtle::Choice;
+#[cfg(verus_keep_ghost)]
+use vstd::arithmetic::mul::lemma_mul_strict_inequality;
+use vstd::calc;
 
 #[cfg(feature = "zeroize")]
 use zeroize::Zeroize;
@@ -90,8 +93,36 @@ fn m(x: u64, y: u64) -> (z: u128)
         z < (1u128 << 104),
         z == x * y,
 {
-    assume(false);
+    proof {
+        lemma_52_52(x, y);
+    }
     (x as u128) * (y as u128)
+}
+
+pub proof fn lemma_52_52(x: u64, y: u64)
+    requires
+        x < (1u64 << 52),
+        y < (1u64 << 52),
+    ensures
+        (x as u128) * (y as u128) < (1u128 << 104),
+{
+    assert(1u128 << 52 == 1u64 << 52) by (bit_vector);
+    calc! {
+        (<)
+        (x as u128) * (y as u128); (<=) {
+            if x > 0 {
+                lemma_mul_strict_inequality(y as int, (1u128 << 52) as int, x as int);
+                assert(x * y < x * (1u128 << 52));
+            } else {
+                assert((0 as u128) * (y as u128) == 0);
+            }
+        }
+        (x as u128) * (1u128 << 52); (<) {
+            lemma_mul_strict_inequality(x as int, (1u128 << 52) as int, (1u128 << 52) as int);
+        }
+        (1u128 << 52) * (1u128 << 52);
+    }
+    assert((1u128 << 52) * (1u128 << 52) == (1u128 << 104)) by (compute);
 }
 
 impl Scalar52 {

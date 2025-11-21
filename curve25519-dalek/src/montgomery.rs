fba162a9612b14c155a73c8620e4e185dfd81f5f
Theodore Ehrenborg <46494248+TheodoreEhrenborg@users.noreply.github.com>
2025-11-21 14:49:32 +0000

    Make a verus mul macro (#328)
    * Make a 2nd macro that uses verus
    
    * Use the macro
    
    * Remove comment
    
    * DRY EdwardsPoint too
    
    * Remove unused code
    
    * Change name
    
    * Re-number
    
    * Format

--- curve25519-dalek/src/montgomery.rs
+++ curve25519-dalek/src/montgomery.rs
@@ -752,26 +752,11 @@ define_mul_variants!(
     Output = MontgomeryPoint
 );
 
-/* ORIGINAL CODE: Scalar * MontgomeryPoint macro commented out because macro-generated code
-   is outside verus! blocks and cannot be used inside verus! blocks.
-   Manual implementations are in mul_specs.rs inside verus! block.
-
-define_mul_variants!(
+define_mul_variants_verus!(
     LHS = Scalar,
     RHS = MontgomeryPoint,
     Output = MontgomeryPoint
 );
-*/
-
-/* ORIGINAL CODE: Scalar * MontgomeryPoint macro call commented out to avoid duplicates
-   The implementations are now manually defined inside the verus! block below
-
-define_mul_variants!(
-    LHS = Scalar,
-    RHS = MontgomeryPoint,
-    Output = MontgomeryPoint
-);
-*/
 
 // NOTE: MulSpecImpl for &MontgomeryPoint * &Scalar moved to mul_specs.rs
 /// Multiply this `MontgomeryPoint` by a `Scalar`.
